71b462c539e01522d65acee221f2671aa6ba62cf
Sara-pyon <149462367+Sara-pyon@users.noreply.github.com>
2023-12-02 23:29:47 -0700

    define active link

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ const navClose = document.getElementById('nav-close');
 
 if(navToggle){
     navToggle.addEventListener('click',() =>{
-        console.log('a');
         navMenu.classList.add('show-menu');
     })
 }
@@ -17,4 +16,42 @@ if(navClose){
     })
 }
 
-// ======================== SCROLLUP SHOW =====================
+// ======================== SHOW SCROLLUP =====================
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scrollUp');
+    window.scrollY >= 350 ? scrollUp.classList.add('show-scrollUp'):
+                            scrollUp.classList.remove('show-scrollUp');
+}
+
+window.addEventListener('scroll', scrollUp);
+
+// ======================== CHANGE MENU BACKGROUND =====================
+const scrollMenu = () =>{
+    const scrollMenu = document.getElementById('header');
+    window.scrollY >= 50 ? scrollMenu.classList.add('scroll-menu'):
+                            scrollMenu.classList.remove('scroll-menu');
+}
+
+window.addEventListener('scroll',scrollMenu);
+
+// ======================== ACTIVE LINK =====================
+const sections = document.querySelectorAll('.section')
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop-200,
+              sectionId = current.getAttribute('id'),
+              sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+    
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        sectionClass.classList.add('active-link');
+    }else{
+        sectionClass.classList.remove('active-link');
+    }
+    })
+}
+
+window.addEventListener('scroll',scrollActive);
